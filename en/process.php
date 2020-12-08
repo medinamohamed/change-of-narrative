c35ac983028ef543f6f69380d6e57c24ed1545e3
Medina <medina.ahmed.mo@gmail.com>
2020-12-08 11:20:03 -0500

    *

---
+++ en/process.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <?php
+
+        if(isset($_POST['form'])) {
+            $firstName = $_POST['first_name'];
+            echo  $firstName ;
+        }
+
+
+    ?>
+    
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Demo at : https://changeofnarrative-demo.netlify.app/
